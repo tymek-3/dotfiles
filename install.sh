6705f8614f9977c034793f20b83814b48c24eb5f
tymek-3 <tymekg3@gmail.com>
2024-10-28 20:09:47 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
+ln -sf ~/dotfiles/nvim  ~/.config/nvim
+ln -sf ~/dotfiles/i3  ~/.config/i3
+ln -sf ~/dotfiles/i3status  ~/.config/i3status
+ln -sf ~/dotfiles/kitty  ~/.config/kitty
