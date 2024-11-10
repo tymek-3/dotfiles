566431760f4e3d7df4ddf7b33cab3d5ca7f1c789
tymek-3 <tymekg3@gmail.com>
2024-11-10 23:17:41 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ else
 	git -C ~/dotfiles/nvim pull
 fi
 
-echo "got here"
 dotfiles=( .zshrc .tmux.conf .ideavimrc .gitconfig )
 for file in ${dotfiles[@]}; do
 	ln -sf ~/dotfiles/${file} ~/${file}
@@ -16,9 +15,12 @@ directories=( nvim i3 i3status kitty )
 for dir in ${directories[@]}; do
 	if [ -L ~/.config/${dir} ]; then
 		unlink ~/.config/${dir}
+        echo "${dir} unlinked"
     elif [ -d ~/.config/${dir} ]; then
 		mv ~/.config/${dir} ~/.config/!old.${dir}
+        echo "~/.config/${dir} renamed to ~/.config/!old.${dir}"
 	fi
 
 	ln -s ~/dotfiles/${dir}  ~/.config/${dir}
+    echo "${dir} linked"
 done
